069043c44b53e33d8cb89063fe8431093bcc0201
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2024-01-21 18:11:45 -0700

    working on front page

--- front-end/budget/app/page.tsx
+++ front-end/budget/app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p className="btn">Hello World!</p>
+    <main>
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Budgeting Application</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
       </div>
     </main>
   );
