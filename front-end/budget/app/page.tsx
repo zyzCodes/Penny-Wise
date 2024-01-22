e3ec5d54aed5ad0dca57372b1055d7ea57d0a65c
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2024-01-21 18:31:08 -0700

    added log-in page

--- front-end/budget/app/page.tsx
+++ front-end/budget/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Budgeting Application</h1>
             <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <Link href="sign-in/">
+              <button className="btn btn-primary">Get Started</button>
+            </Link>
           </div>
         </div>
       </div>
